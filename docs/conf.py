4a3b78b4300fd257bd6e8d017721478a7f3cb0ba
Jordana_Lindner <jordana.lindnerovadia@weizmann.ac.il>
2024-01-07 15:34:45 +0200

    fix ModuleNotFoundError: No module named 'mock'

--- docs/conf.py
+++ docs/conf.py
@@ -57,7 +57,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = u'Python3.11'
+language = u'Python2.7'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
