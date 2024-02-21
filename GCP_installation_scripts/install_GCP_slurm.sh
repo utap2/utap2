ac98ac3cdb489a93b85bb9f8d53a0e830f3b096d
Jordana_Lindner <jordana.lindnerovadia@weizmann.ac.il>
2024-02-21 11:00:01 +0200

    fix bugs

--- GCP_installation_scripts/install_GCP_slurm.sh
+++ GCP_installation_scripts/install_GCP_slurm.sh
@@ -49,10 +49,7 @@ export project_num=`gcloud projects describe  $project_id --format="value(projec
 
 echo "project id: $project_id, project num: $project_num, bucket name: $bucket_name" 
 
-#override_optional_param "GCP" "1"
-override_optional_param "GCP_BUCKET" "$bucket_name"
-#override_optional_param "MAIL_PASSWORD" "zhytvkqzokgoarsl"
-#override_optional_param "CLUSTER_TYPE" "slurm"
+
 
 #set default project 
 #gcloud auth login - you are allready authenticated with gcloud when running inside the Cloud Shell and so do not need to run this command.
@@ -136,7 +133,7 @@ gcloud projects add-iam-policy-binding $project_id \
    --role='roles/compute.networkUser'
    
 
-gcloud storage cp ~/utap2/scripts/* gs://$bucket_name
+
 
 #download all installation files from dors4 to bucket, this option takes very long time and only available if transfer data service is functional
 mkdir "$HOME/data" && gcsfuse -o rw -file-mode=777 -dir-mode=777 --implicit-dirs $bucket_name "$HOME/data"
@@ -186,6 +183,14 @@ sleep 100
 #copy ssh files from host to login VM 
 export USER_LOGIN=`gcloud compute os-login describe-profile --format json|jq -r '.posixAccounts[].username'`
 export LOGIN_IP=`gcloud compute instances list --sort-by=~creationTimestamp --format="value(EXTERNAL_IP)" | head -n 1`
+
+#override_optional_param "GCP" "1"
+override_optional_param "GCP_BUCKET" "$bucket_name"
+override_optional_param "DNS_HOST" "$LOGIN_IP"
+#override_optional_param "MAIL_PASSWORD" "zhytvkqzokgoarsl"
+#override_optional_param "CLUSTER_TYPE" "slurm"
+gcloud storage cp ~/utap2/scripts/* gs://$bucket_name
+
 rm ~/.ssh/known_hosts
 ssh -i ~/.ssh/google_compute_engine -o StrictHostKeyChecking=no -l $USER_LOGIN $LOGIN_IP "mkdir ~/.ssh;"
 scp -i ~/.ssh/google_compute_engine ~/.ssh/google_compute_engine "$USER_LOGIN"@"$LOGIN_IP":.ssh/id_rsa
