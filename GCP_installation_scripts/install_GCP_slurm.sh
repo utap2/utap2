e050498ec8ed45162475ed9a7906ba4f7099c2db
utap2 <130136775+utap2@users.noreply.github.com>
2024-03-19 13:27:56 +0200

    Update install_GCP_slurm.sh

--- GCP_installation_scripts/install_GCP_slurm.sh
+++ GCP_installation_scripts/install_GCP_slurm.sh
@@ -5,7 +5,7 @@
 #A project connected the billing account  
 
 
-#Genarate a project an associate it to a billing account  
+# Genarate a project an associate it to a billing account  
 #export project_id="utap-$TIME_STEMP"
 #gcloud projects create $project_id
 #wait $!
@@ -15,7 +15,9 @@ override_optional_param () {
 } 
 
 
-#get project id as parameter, and bucket name if exist
+
+# Get project id as parameter, and bucket name if exist. 
+# The bucket will contain all UTAP installation files (genomes are already found in the controller image together with UTAP installation as utap.sandbox )
 while getopts "b:i:" option; do
   case "${option}" in
     i) export project_id=${OPTARG};;
@@ -36,7 +38,7 @@ fi
 
 
 
-# Check if bucket name is provided 
+# Check if bucket name is provided, if not, create one 
 if [ -z "$bucket_name" ]; then
   export TIME_STEMP=`date +"%d-%m-%Y-%H-%M-%S"`
   export bucket_name="utap-data-$TIME_STEMP"
@@ -44,6 +46,7 @@ if [ -z "$bucket_name" ]; then
   wait $!
 fi
 
+# Get project number
 export project_num=`gcloud projects describe  $project_id --format="value(projectNumber)"`
 
 
@@ -51,31 +54,31 @@ echo "project id: $project_id, project num: $project_num, bucket name: $bucket_n
 
 
 
-#set default project 
+# Set default project 
 #gcloud auth login - you are allready authenticated with gcloud when running inside the Cloud Shell and so do not need to run this command.
 gcloud config set project $project_id
 
-#grant user access credentials to terraform 
+# Grant user access credentials to terraform 
 gcloud auth application-default login
 
-#enable VM connection to your cluster by using ssh with os-login at project level
+# Enable VM connection to your cluster by using ssh with os-login at project level
 gcloud compute project-info add-metadata --metadata enable-oslogin=TRUE
 
 
-#Grant required roles to your user account
-#grant the roles/storage.admin role
+# Grant required roles to your user account
+# Grant the roles/storage.admin role
 gcloud projects add-iam-policy-binding $project_id \
   --member="user:$USER_EMAIL" \
   --role='roles/storage.admin'
-#Grant the roles/viewer role
+# Grant the roles/viewer role
 gcloud projects add-iam-policy-binding $project_id \
   --member="user:$USER_EMAIL" \
   --role='roles/viewer'
-#Grant the roles/resourcemanager.projectIamAdmin role
+# Grant the roles/resourcemanager.projectIamAdmin role
 gcloud projects add-iam-policy-binding $project_id \
   --member="user:$USER_EMAIL" \
   --role='roles/resourcemanager.projectIamAdmin'
-#Grant the roles/cloudbuild.builds.editor role
+# Grant the roles/cloudbuild.builds.editor role
 gcloud projects add-iam-policy-binding $project_id \
   --member="user:$USER_EMAIL" \
   --role='roles/cloudbuild.builds.editor'
@@ -83,51 +86,51 @@ gcloud projects add-iam-policy-binding $project_id \
 
 
 
-#Grant required roles to the Compute Engine service account
-#enable the default Compute Engine service account.
+# Grant required roles to the Compute Engine service account
+# Enable the default Compute Engine service account.
 gcloud iam service-accounts enable --project $project_id $project_num-compute@developer.gserviceaccount.com
 gcloud projects add-iam-policy-binding $project_id --member=serviceAccount:$project_num-compute@developer.gserviceaccount.com --role=roles/editor
 
-#grant the roles/editor IAM role to the service account.
+#  Grant the roles/editor IAM role to the service account.
 gcloud iam service-accounts enable --project $project_id $project_num-compute@developer.gserviceaccount.com
 
-#enable storage transfer service account iams 
+# Enable storage transfer service account iams 
 gcloud projects add-iam-policy-binding $project_id --member=serviceAccount:project-$project_num@storage-transfer-service.iam.gserviceaccount.com --role=roles/storage.objectAdmin
 gcloud projects add-iam-policy-binding $project_id --member=serviceAccount:project-$project_num@storage-transfer-service.iam.gserviceaccount.com --role=roles/owner
 
-#grant the roles/compute.storageAdmin role
+# Grant the roles/compute.storageAdmin role
 gcloud projects add-iam-policy-binding $project_id \
   --member="serviceAccount:$project_num-compute@developer.gserviceaccount.com" \
   --role='roles/compute.storageAdmin'
 
 
-#if you're importing an image, grant the roles/storage.objectViewer role
+# If you're importing an image, grant the roles/storage.objectViewer role
 gcloud projects add-iam-policy-binding $project_id \
   --member="serviceAccount:$project_num-compute@developer.gserviceaccount.com" \
   --role='roles/storage.objectViewer'
   
-#if you're exporting an image, grant the roles/storage.objectAdmin role
+# If you're exporting an image, grant the roles/storage.objectAdmin role
 gcloud projects add-iam-policy-binding $project_id \
   --member="serviceAccount:$project_num-compute@developer.gserviceaccount.com" \
   --role='roles/storage.objectAdmin'
 
 
-#grant the roles/compute.admin role to the Cloud Build service account. 
+# Grant the roles/compute.admin role to the Cloud Build service account. 
 gcloud projects add-iam-policy-binding $project_id \
    --member="serviceAccount:$project_num@cloudbuild.gserviceaccount.com" \
    --role='roles/compute.admin'
 
-#grant the roles/iam.serviceAccountUser role
+# Grant the roles/iam.serviceAccountUser role
 gcloud projects add-iam-policy-binding $project_id \
    --member="serviceAccount:$project_num@cloudbuild.gserviceaccount.com" \
    --role='roles/iam.serviceAccountUser'
    
-#grant the roles/iam.serviceAccountTokenCreator role
+# Grant the roles/iam.serviceAccountTokenCreator role
 gcloud projects add-iam-policy-binding $project_id \
    --member="serviceAccount:$project_num@cloudbuild.gserviceaccount.com" \
    --role='roles/iam.serviceAccountTokenCreator'
 
-#grant the roles/compute.networkUser role
+# Grant the roles/compute.networkUser role
 gcloud projects add-iam-policy-binding $project_id \
    --member="serviceAccount:$project_num@cloudbuild.gserviceaccount.com" \
    --role='roles/compute.networkUser'
@@ -135,9 +138,10 @@ gcloud projects add-iam-policy-binding $project_id \
 
 
 
-#download all installation files from dors4 to bucket, this option takes very long time and only available if transfer data service is functional
+# Fuse google bucket to ~/data on google shell 
 mkdir "$HOME/data" && gcsfuse -o rw -file-mode=777 -dir-mode=777 --implicit-dirs $bucket_name "$HOME/data"
 
+# If the project doesn’t already contain UTAP custom images for the login and controller nodes: utap-login-slurm-simple-latest and utap-controller-slurm-simple-latest respectively then you will have to download  the images from dors4 and import them to your google project
 export utap_login=$(gcloud compute images list | grep "utap-login")
 export utap_controller=$(gcloud compute images list | grep "utap-controller")
 
@@ -146,7 +150,7 @@ if [ -z "$utap_login" ] && [ -z "$utap_controller" ]; then
     curl "https://dors4.weizmann.ac.il/utap/UTAP_installation_files/GCP_slurm_cluster/utap-login-slurm-simple-final.tar.gz" | gsutil -h "Cache-Control: no-cache, max-age=0" cp - gs://$bucket_name/utap-login-slurm-simple-final.tar.gz  &
     curl "https://dors4.weizmann.ac.il/utap/UTAP_installation_files/GCP_slurm_cluster/utap-controller-slurm-simple-final.tar.gz" | gsutil -h "Cache-Control: no-cache, max-age=0" cp - gs://$bucket_name/utap-controller-slurm-simple-final.tar.gz &
     wait 
-    #convert vmdk files to bootable images availble on GCP (public images are also availble but can only be stored in private account)
+    #export compressed raw images files to bootable images availble on GCP 
     gcloud compute images create utap-controller-slurm-simple-final --source-uri gs://$bucket_name/utap-controller-slurm-simple-final.tar.gz
     gcloud compute images create utap-login-slurm-simple-final --source-uri gs://$bucket_name/utap-login-slurm-simple-final.tar.gz
     #gcloud compute images import utap-controller --source-file gs://$bucket_name/utap-controller-slurm-simple-final.tar.gz --timeout=24h &
@@ -154,25 +158,30 @@ if [ -z "$utap_login" ] && [ -z "$utap_controller" ]; then
     wait
 fi
 
+# Not in use - VMDK files are larger then compressed raw tar.gz images and downloadng them takes longer, therefore VMDK images are not used for the installation
 #wget -nH -nc -P ~/data/genomes https://dors4.weizmann.ac.il/utap/UTAP_genomes/All_genomes/*  && wget -nH -nc -P ~/data https://dors4.weizmann.ac.il/utap/UTAP_installation_files/GCP_slurm_cluster/utap-controller-final.vmdk && wget -nH -nc -P ~/data https://dors4.weizmann.ac.il/utap/UTAP_installation_files/GCP_slurm_cluster/utap-login-final.vmdk 
 
-#transfer all installation files from dors4 to bucket using data tansfer service 
+# Transfer all installation files from dors4 to bucket using data tansfer service - not in use since google transfer service from dors4 to google bucket is very slow - 3GB per hour
 #gcloud transfer jobs create https://dors4.weizmann.ac.il/utap/data_transfer_tsv_files/tsv.txt gs://$bucket_name
 #data_id=`gcloud transfer jobs list | grep NAME | head -n 1 | awk '{print $2}'`
 #gcloud transfer jobs monitor $data_id
 
 
-#clone the Cloud HPC Toolkit GitHub repository - not in use
+# Clone the Cloud HPC Toolkit GitHub repository - not in use
 #sed -i "s/project_id: .*/project_id: '${project_id//\//\\/}'/" ~/utap2/GCP_installation_scripts/hpc-slurm-utap.yaml
 #cd ~ && git clone https://github.com/GoogleCloudPlatform/hpc-toolkit.git
 #cd hpc-toolkit/ && make 
 #./ghpc create -w ~/utap2/GCP_installation_scripts/hpc-slurm-utap.yaml && ./ghpc deploy hpc-utap
 
-#clone the Cloud SchedMD  GitHub repository and install slurm cluster with terraform
+# Clone the Cloud SchedMD  GitHub repository
 cd ~ && git clone https://github.com/utap2/slurm-gcp.git
+
+# Change the project id in the relevant slurm-gcp files. 
 sed -i "s/project_id = .*/project_id = \"${project_id//\//\\/}\"/" ~/slurm-gcp/terraform/slurm_cluster/examples/slurm_cluster/simple_cloud_utap/example.tfvars
 sed -i "s/PROJECT_ID/${project_id//\//\\/}/" ~/slurm-gcp/terraform/slurm_cluster/examples/slurm_cluster/simple_cloud_utap/main.tf
 sed -i "s/BUCKET_NAME/${bucket_name//\//\\/}/" ~/slurm-gcp/terraform/slurm_cluster/examples/slurm_cluster/simple_cloud_utap/main.tf
+
+# Install slurm cluster with terraform
 cd ~/slurm-gcp/terraform/slurm_cluster/examples/slurm_cluster/simple_cloud_utap
 terraform init && terraform validate && terraform apply -var-file=example.tfvars || (echo "ERROR installing GCP Slurm cluster")
 wait
@@ -181,12 +190,13 @@ if [ $STATUS -ne 0 ]; then
     echo "Error: One or more processes failed with exit status $STATUS"
     exit $STATUS
 fi
-sleep 100
-#copy ssh files from host to login VM 
-export home_basename="$(basename "$HOME")"
-export USER_LOGIN="$home_basename""_gmail_com"
+sleep 100 # wait for slurm cluster to be configured 
+
+
+export USER_LOGIN="$USER""_gmail_com"
 export LOGIN_IP=`gcloud compute instances list --sort-by=~creationTimestamp --format="value(EXTERNAL_IP)" | head -n 1`
 
+# Override parameters in optional_parametrs.conf and copy the modified files to the bucket
 #override_optional_param "GCP" "1"
 override_optional_param "GCP_BUCKET" "$bucket_name"
 override_optional_param "DNS_HOST" "$LOGIN_IP"
@@ -194,15 +204,22 @@ override_optional_param "DNS_HOST" "$LOGIN_IP"
 #override_optional_param "CLUSTER_TYPE" "slurm"
 gcloud storage cp ~/utap2/scripts/* gs://$bucket_name
 
-rm ~/.ssh/known_hosts
+# Copy ssh files from the host - google shell to login node 
+printf 'Y\n' | gcloud compute ssh --zone "us-central1-a" "simple-login-l0-001" --project $project_id &
+sleep 10
 ssh -i ~/.ssh/google_compute_engine -o StrictHostKeyChecking=no -l $USER_LOGIN $LOGIN_IP "mkdir ~/.ssh;"
 scp -i ~/.ssh/google_compute_engine ~/.ssh/google_compute_engine "$USER_LOGIN"@"$LOGIN_IP":.ssh/id_rsa
 scp -i ~/.ssh/google_compute_engine ~/.ssh/google_compute_engine.pub "$USER_LOGIN"@"$LOGIN_IP":.ssh/id_rsa.pub
+
+# Mount bucket to $HOME/data on the login node. The bucket will contain all UTAP installation files (genomes are already found in the controller image together with UTAP installation as utap.sandbox ) and run UTAP
 ssh -i  ~/.ssh/google_compute_engine  "$USER_LOGIN"@"$LOGIN_IP" "mkdir -p ~/data && gcsfuse -o rw -file-mode=777 -dir-mode=777 --debug_fuse_errors  --debug_fuse --debug_fs --debug_gcs --implicit-dirs \"$bucket_name\" ~/data && bash data/install_UTAP_singularity.sh -a data/required_parameters.conf -b data/optional_parameters.conf"
+
+# Show the user the login URL to UTAP
 sed -i 's/\r$//'  ~/utap2/scripts/optional_parameters.conf
 source  ~/utap2/scripts/optional_parameters.conf
 echo "UTAP has been installed successfully. Please visit the site http://$LOGIN_IP:$HOST_APACHE_PORT"
-#enter login VM
+
+# Enter login node
 #export GOOGLE_CLOUD_PROJECT=`gcloud config list --format 'value(core.project)'`
 #export REGION=`gcloud config list --format 'value(compute.region)'`
 #export ZONE=`gcloud config list --format 'value(compute.zone)'`
